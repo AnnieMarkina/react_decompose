1a53de810400cc30e2315bba9824de2510e4f9f5
AnnieMarkina <anniemurkina@gmail.com>
2020-02-13 14:15:56 +0200

    Updates

---
+++ precache-manifest.169954e37e4ff78285dc8723c515f627.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b174cfcefce61771e200efeea9453277",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3daff849f2c5ed6a1866",
+    "url": "./static/css/main.03ccbbfb.chunk.css"
+  },
+  {
+    "revision": "5fdac0a2c9586d9d1584",
+    "url": "./static/js/2.6b91d4b6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.6b91d4b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3daff849f2c5ed6a1866",
+    "url": "./static/js/main.d8338fbe.chunk.js"
+  },
+  {
+    "revision": "e67b79006193623cc7c8",
+    "url": "./static/js/runtime-main.a19c704a.js"
+  }
+]);
